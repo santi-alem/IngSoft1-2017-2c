29879641afe956bd077429e900c5b41ee23e2cb8
santi-alem <santialem.trev@gmail.com>
2017-09-02 13:41:34 -0300

    Ejercicio 1 c#

---
+++ 01-CodigoRepetido/C#/Ejercicio/CustomerBook.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IdiomExercise
+{
+    public class CustomerBook
+    {
+        public static String CUSTOMER_NAME_EMPTY = "Customer name can not be empty";
+        public static String CUSTOMER_ALREADY_EXISTS = "Customer already exists";
+        public static string INVALID_CUSTOMER_NAME = "Invalid Customer name";
+
+        private IList<String> customerNames = new List<String>();
+
+        public void addCustomerNamed(String name)
+        {
+            if (name.Length == 0) throw new Exception(CUSTOMER_NAME_EMPTY);
+            if (containsCustomerNamed(name)) throw new Exception(CUSTOMER_ALREADY_EXISTS);
+
+            customerNames.Add(name);
+        }
+
+        public bool isEmpty()
+        {
+            return customerNames.Count == 0;
+        }
+
+        public int numberOfCustomers()
+        {
+            return customerNames.Count;
+        }
+
+        public bool containsCustomerNamed(String name)
+        {
+            return customerNames.Contains(name);
+        }
+
+        public void removeCustomerNamed(String name)
+        {
+            if (!customerNames.Remove(name))
+                throw new InvalidOperationException(INVALID_CUSTOMER_NAME);
+        }
+    }
+
+}
---
+++ 01-CodigoRepetido/C#/Ejercicio/Test.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IdiomExercise
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void AddingCustomerShouldNotTakeMoreThan50Milliseconds()
+        {
+            CustomerBook customerBook = new CustomerBook();
+
+            DateTime timeBeforeRunning = DateTime.Now;
+            customerBook.addCustomerNamed("John Lennon");
+            DateTime timeAfterRunning = DateTime.Now;
+
+            Assert.IsTrue(timeAfterRunning.Subtract(timeBeforeRunning).TotalMilliseconds < 50);
+        }
+
+        [TestMethod]
+        public void RemovingCustomerShouldNotTakeMoreThan100Milliseconds()
+        {
+            CustomerBook customerBook = new CustomerBook();
+            String paulMcCartney = "Paul McCartney";
+
+            customerBook.addCustomerNamed(paulMcCartney);
+
+            DateTime timeBeforeRunning = DateTime.Now;
+            customerBook.removeCustomerNamed(paulMcCartney);
+            DateTime timeAfterRunning = DateTime.Now;
+
+            Assert.IsTrue(timeAfterRunning.Subtract(timeBeforeRunning).TotalMilliseconds < 100);
+        }
+
+        [TestMethod]
+        public void CanNotAddACustomerWithEmptyName()
+        {
+
+            CustomerBook customerBook = new CustomerBook();
+
+            try
+            {
+                customerBook.addCustomerNamed("");
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(e.Message, CustomerBook.CUSTOMER_NAME_EMPTY);
+                Assert.IsTrue(customerBook.isEmpty());
+            }
+        }
+
+        [TestMethod]
+        public void CanNotRemoveNotAddedCustomer()
+        {
+            CustomerBook customerBook = new CustomerBook();
+            
+            try
+            {
+                customerBook.removeCustomerNamed("John Lennon");
+                Assert.Fail();
+            }
+            // Se utiliza otro tipo de exception por motivos del ejercicio
+            catch (InvalidOperationException e)
+            {
+                Assert.AreEqual(e.Message, CustomerBook.INVALID_CUSTOMER_NAME);
+                Assert.AreEqual(0, customerBook.numberOfCustomers());
+            }
+
+        }
+    }
+}
