52c071208eece310651f4e66c63b1d2195449ace
Santi Alem <santialem@Carloss-MacBook-Pro.local>
2017-09-05 12:32:21 -0300

    Test 1 y 2 codigo remplazado

--- 01-CodigoRepetido/Python/Ejercicio/idiom/IdionTest.py
+++ 01-CodigoRepetido/Python/Ejercicio/idiom/IdionTest.py
@@ -46,49 +46,76 @@ class CustomerBook:
         self.customerNames.remove(name)
 
 
-class StopWatch:
-    def __init__(self):
-        self.start_time = 0
-        self.end_time = 0
+# # la clase esta no se si es un toque demasiado
+# class StopWatch:
+#     def __init__(self):
+#         self.start_time = 0
+#         self.end_time = 0
+#
+#     def start(self):
+#         self.start_time = time.time()
+#
+#     def stop(self):
+#         self.end_time = time.time()
+#
+#     @property
+#     def time_elapsed(self):
+#         return self.end_time - self.start_time
+#
+#     ## A partir de aaca  Esto ya no se si es la responsabilidad del Objecto
+#     def time_function(self, function_to_time):
+#         self.start()
+#         function_to_time()
+#         self.stop()
+#
+#     def less_than_ms(self, time_in_ms):
+#         return (self.time_elapsed) * 1000 < time_in_ms
+#
+#
+# def functionTakesLessTimeThan(function, time_in_ms):
+#     watch = StopWatch()
+#     watch.time_function(function)
+#     return watch.less_than_ms(time_in_ms)
 
-    def start(self):
-        self.start_time = time.time()
 
-    def stop(self):
-        self.end_time = time.time()
-
-    @property
-    def time_elapsed(self):
-        return self.end_time - self.start_time
-
-    ## A partir de aaca  Esto ya no se si es la responsabilidad del Objecto
-    def time_function(self, function_to_time):
-        self.start()
-        function_to_time()
-        self.stop()
-
-    def less_than_ms(self, time_in_ms):
-        return (self.time_elapsed) * 1000 < time_in_ms
+def functionTakeLessTime(function_to_time, time_in_ms):
+    time_elapsed = timeFunction(function_to_time)
+    return lessThanInMs(time_elapsed, time_in_ms)
 
 
-def functionTakesLessTimeThan(function, time_in_ms):
-    watch = StopWatch()
-    watch.time_function(function)
-    return watch.less_than_ms(50)
+def lessThanInMs(time_elapsed, time_in_ms):
+    return (time_elapsed) * 1000 < time_in_ms
+
+
+def timeFunction(function_to_time):
+    start_time = time.time()
+    function_to_time()
+    end_time = time.time()
+    return end_time - start_time
 
 
 class IdionTest(unittest.TestCase):
+
+    def failUnlessException(self, function_to_test, error, assert_expression):
+        try:
+            function_to_test()
+            self.Fail()
+        except error as exception:
+            assert_expression(exception)
+        
+
+
     def testAddingCustomerShouldNotTakeMoreThan50Milliseconds(self):
         customerBook = CustomerBook()
 
-        self.assertTrue(functionTakesLessTimeThan(lambda: customerBook.addCustomerNamed('John Lennon'), 50))
+        self.assertTrue(functionTakeLessTime(lambda: customerBook.addCustomerNamed('John Lennon'), 50))
 
     def testRemovingCustomerShouldNotTakeMoreThan100Milliseconds(self):
         customerBook = CustomerBook()
 
         customerBook.addCustomerNamed('Paul McCartney')
 
-        self.assertTrue(functionTakesLessTimeThan(lambda: customerBook.removeCustomerNamed('Paul McCartney'), 100))
+        self.assertTrue(functionTakeLessTime(lambda: customerBook.removeCustomerNamed('Paul McCartney'), 100))
 
     def testCanNotAddACustomerWithEmptyName(self):
         customerBook = CustomerBook()
