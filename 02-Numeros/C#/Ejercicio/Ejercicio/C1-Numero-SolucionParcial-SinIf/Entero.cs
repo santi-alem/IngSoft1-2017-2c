8dbe5683b37a1bdbf1e9264adbd92927b3174ea8
Mattenet, Pedro <pedro.mattenet@WAYNE.COM>
2017-09-09 05:30:11 -0300

    no se bien que hice aca, rollbackeamos de ultima

---
+++ 02-Numeros/C#/Ejercicio/Ejercicio/C1-Numero-SolucionParcial-SinIf/Entero.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace C1_Numero_SolucionParcial_SinIf
+{
+    public class Entero: Numero
+    {
+    	protected int value;
+	
+	    public Entero(int value){
+		    this.value = value;
+	    }
+	
+	    public int getValue(){
+		    return value;
+	    }
+	
+	    override public Numero mas(Numero sumando) {
+            if (sumando is Entero) 
+                return new Entero(value + ((Entero) sumando).getValue());
+            else if (sumando is Fraccion)
+            {
+                Numero nuevoNumerador = this.por(((Fraccion) sumando).getDenominador().mas(((Fraccion)sumando).getNumerador()));
+                return Fraccion.dividir((Entero) nuevoNumerador, ((Fraccion) sumando).getDenominador());
+            }
+            else 
+               throw new SystemException();
+        }
+
+	    override public Numero por(Numero multiplicador) {
+            if (multiplicador is Entero)
+                return new Entero(value * ((Entero) multiplicador).getValue());
+            else if (multiplicador is Fraccion)
+            {
+                return Fraccion.dividir((Entero)((Fraccion) multiplicador).getNumerador().por(this), ((Fraccion) multiplicador).getDenominador());
+            }
+            else
+                throw new SystemException();
+        }
+	
+        override public Numero dividido(Numero divisor) {
+            return Fraccion.dividir(this, (Entero) divisor);
+        }
+	
+        public Entero maximoComunDivisorCon(Entero otroEntero)
+        {
+            if (otroEntero.esCero())
+                return this;
+            else
+                return otroEntero.maximoComunDivisorCon(this.restoCon(otroEntero));
+        }
+
+        public Entero restoCon(Entero divisor)
+        {
+            return new Entero(value % divisor.getValue());
+        }
+
+        public Entero divisionEntera(Entero divisor)
+        {
+            return new Entero(value / divisor.getValue());
+        }
+	    
+	    override public bool esCero () {
+		    return value == 0;
+	    }
+	    
+	    override public bool esUno() {
+		    return value == 1;
+	    }
+	    
+	    override public bool Equals(Object anObject){
+		    if (typeof(Entero)==anObject.GetType())
+			    return value==((Entero) anObject).getValue();
+		    else
+			    return false;
+	    }
+
+        override public int GetHashCode()
+        {    
+		    return value;
+	    }
+    }
+}
---
+++ 02-Numeros/C#/Ejercicio/Ejercicio/C1-Numero-SolucionParcial-SinIf/Fraccion.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace C1_Numero_SolucionParcial_SinIf 
+{
+    public class Fraccion: Numero
+    {
+	    protected Entero numerador;
+	    protected Entero denominador;
+	
+	    public static Numero dividir(Entero dividendo, Entero divisor ) {
+		
+		    if(divisor.esCero()) throw new Exception (Numero.DESCRIPCION_DE_ERROR_NO_SE_PUEDE_DIVIDIR_POR_CERO);
+		    if(dividendo.esCero()) return dividendo;
+		
+		    Entero maximoComunDivisor = dividendo.maximoComunDivisorCon(divisor);
+		    Entero numerador = dividendo.divisionEntera(maximoComunDivisor);
+		    Entero denominador = divisor.divisionEntera(maximoComunDivisor);
+		
+		    if (denominador.esUno()) return numerador;
+		
+		    return new Fraccion(numerador,denominador);
+
+	    }
+	
+	    private Fraccion(Entero numerador, Entero denominador){
+		    this.numerador = numerador;
+		    this.denominador = denominador;
+	    }
+	
+	    public Entero getNumerador (){
+		    return numerador;
+	    }
+	
+	    public Entero getDenominador(){
+		    return denominador;
+	    }
+
+	    override public bool esCero() {
+		    return false;
+	    }
+
+	    override public bool esUno() {
+		    return false;
+	    }
+	    
+	    override public bool Equals(Object anObject){
+		    if (typeof(Fraccion)==anObject.GetType()) 
+			    return equals((Fraccion) anObject);
+		    else
+			    return false;
+	    }
+	
+	    public bool equals(Fraccion aFraccion){
+		    return numerador.por(aFraccion.getDenominador()).Equals( denominador.por(aFraccion.getNumerador()));
+	    }
+	    
+	    override public int GetHashCode() {
+		    return numerador.GetHashCode() / denominador.GetHashCode();
+	    }
+	    
+	    override public Numero mas(Numero sumando) {
+	        if (sumando is Fraccion)
+	        {
+	            Fraccion sumandoComoFraccion = (Fraccion) sumando;
+	            Numero nuevoDenominador = denominador.por(sumandoComoFraccion.getDenominador());
+	            Numero nuevoNumerador1 = numerador.por(sumandoComoFraccion.getDenominador());
+	            Numero nuevoNumerador2 = denominador.por(sumandoComoFraccion.getNumerador());
+	            Numero nuevoNumerador = nuevoNumerador1.mas(nuevoNumerador2);
+	            return nuevoNumerador.dividido(nuevoDenominador);
+            }
+            else if (sumando is Entero)
+	        {
+	            Entero sumandoComoEntero = (Entero)sumando;
+	            Entero nuevoNumerador = (Entero) denominador.por(sumandoComoEntero).mas(numerador);
+	            
+	            return dividir(nuevoNumerador, denominador);
+            }
+            else 
+                throw new SystemException();
+	        
+        }
+	
+	    override public Numero por(Numero multiplicador) {
+            Fraccion multiplicadorComoFraccion = (Fraccion)multiplicador;
+            return numerador.por(multiplicadorComoFraccion.getNumerador()).
+                dividido(denominador.por(multiplicadorComoFraccion.getDenominador()));
+        }
+	
+	    override public Numero dividido(Numero divisor) {
+            Fraccion divisorComoFraccion = (Fraccion)divisor;
+            return numerador.por(divisorComoFraccion.getDenominador()).
+                dividido(denominador.por(divisorComoFraccion.getNumerador()));
+        }
+
+    }
+}
